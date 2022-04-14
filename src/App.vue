cd3bdc729ecafa518e876e2fccd462153f58097c
Beryl-18 <beryla.s@hotmail.com>
2022-04-14 15:47:14 +0400

    refactoring appvue with logic and checkout button

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,25 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="stylesheet.css">
     <div id = "lessonplanapp">
-        <main>
-            <lesson-list @addLesson="addToCart" ></lesson-list>
+            <!-- Navbar -->
+            <div id="navbar">
+                <!-- Shoppping Cart Button -->
+                <div>
+                    <button class="checkoutCart" v-if="cartAggregate > 0" @click="showCheckout()">
+                        <!-- create logic to switch between checkout and lesson plan? -->
+                        Checkout <span class="fas fa-cart-plus"></span> {{cartAggregate}}
+                    </button>
+                    <button class="checkoutCart" v-else disabled>
+                        <!-- create logic to switch between checkout and lesson plan? -->
+                         Checkout <span class="fas fa-cart-plus"></span>
+                    </button>
+                </div>
+            </div>
 
-            <checkout :cart="cart" ></checkout>
+        <main>
+            <lesson-list v-if="checkoutLogic" @addLesson="addToCart" ></lesson-list>
+
+            <checkout v-else :cart="cart" ></checkout>
         </main>
     </div>
 </div>
@@ -24,13 +39,39 @@ export default {
     },
     data(){
         return{
-            cart: []
+            cart: [],
+            checkoutLogic : true
         }
     },
     methods:{
-        addToCart(){
-
+    //Function to push items to cart
+        addtoCart(course){
+            let lessonIncart = this.cart.find(i => i.id == course.id);
+            if (lessonIncart){
+                lessonIncart.quantity++;
+                course.spaces = course.spaces - 1;
+                }
+            else {
+                this.cart.push(course);
+                course.quantity++;
+                course.spaces = course.spaces - 1;
+            }
+       },
+    showCheckout() {
+        this.checkoutLogic ? this.checkoutLogic = false : this.checkoutLogic = true;
+        }
+    },
+    computed:{
+        cartAggregate(){
+            let itemcounter = 0;
+                if (this.cart.length > 0){
+                    for (let i = 0; i < this.cart.length; i++){
+                        itemcounter += this.cart[i].quantity;
+                    }
+                }
+            return itemcounter;
         }
     }
+    
 }
 </script>
